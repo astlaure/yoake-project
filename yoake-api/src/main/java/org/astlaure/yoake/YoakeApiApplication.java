10918a2fd0f78514b5670471c3a9e5b9040b2e07
Alexandre St-Laurent <alexandre.stlaurent.4@gmail.com>
2022-03-19 12:38:24 -0400

    initial commit 🎉

---
+++ yoake-api/src/main/java/org/astlaure/yoake/YoakeApiApplication.java
@@ -0,0 +1,13 @@
+package org.astlaure.yoake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoakeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoakeApiApplication.class, args);
+    }
+
+}
---
+++ yoake-api/src/test/java/org/astlaure/yoake/YoakeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.astlaure.yoake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoakeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
